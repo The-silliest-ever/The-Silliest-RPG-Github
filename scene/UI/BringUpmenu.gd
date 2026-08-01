9fa0a2b626a7ada1f3fac1d5e6bd5f20f87cd3ce
Silliest Official <thesilliestemailofficial@gmail.com>
2026-08-01 16:10:50 -0400

    Started prepping for a menu

---
+++ scene/UI/BringUpmenu.gd
@@ -0,0 +1,8 @@
+extends Button
+
+const UI_toLoad = preload("res://scene/UI/GenMenu.tscn")
+
+func _on_pressed() -> void:
+	var ui_instance = UI_toLoad.instantiate()
+	
+	add_child(ui_instance)
--- scripts/button.gd
+++ scripts/button.gd
@@ -2,3 +2,7 @@ extends Button
 
 func _on_pressed() -> void:
 	get_tree().quit()
+
+
+func _on_menu_pressed() -> void:
+	pass # Replace with function body.
